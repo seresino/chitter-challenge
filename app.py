dc701293d04b26ea8c9334d5a4ee83203d345abd
Ruby <rubyseresin@gmail.com>
2023-12-07 15:34:15 +0000

    set up route and template to display all peeps

--- app.py
+++ app.py
@@ -6,26 +6,39 @@ from lib.peep import *
 from peewee import *
 from datetime import *
 
-# Create a new Flask app
-app = Flask(__name__)
+# Create a function to create and configure the Flask app
+def create_app():
+    app = Flask(__name__)
 
-# Define your Peewee database instance
-db = PostgresqlDatabase(
-    'chitter-challenge',  # Your database name
-    user='rubyseresin',  # Your PostgreSQL username
-    password='',  # Your PostgreSQL password
-    host='localhost'  # Your PostgreSQL host
-)
+    # Define your Peewee database instance
+    db = PostgresqlDatabase(
+        'chitter-challenge',  # Your database name
+        user='rubyseresin',  # Your PostgreSQL username
+        password='',  # Your PostgreSQL password
+        host='localhost'  # Your PostgreSQL host
+    )
 
-# Connect to the database
-db.connect()
-new_user = Person(name='barry', email='barry@example.com', password='password')
-new_peep = Peep(content='First Peep!', user_id=1)
-new_user.save()
-new_peep.save()
+    # Initialize the database connection in the Flask app context
+    @app.before_request
+    def before_request():
+        db.connect()
 
-# == Your Routes Here ==
+    @app.after_request
+    def after_request(response):
+        db.close()
+        return response
 
+    # Define your routes
+    @app.route('/', methods=['GET'])
+    def get_peeps():
+        peeps = Peep.select()
+        return render_template('index.html', peeps=peeps)
+    
+
+    return app
+
+# Create the Flask app using the create_app function
+app = create_app()
 
 
 # These lines start the server if you run this file directly
